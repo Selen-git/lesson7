9dd32d499034ce36420f97237aa06a17ff2078b5
Selen-git <Selen_6966@inbox.ru>
2025-03-27 01:47:12 +0500

    Update Autotest internet shop.py

--- 07_lesson/Autotest internet shop.py
+++ 07_lesson/Autotest internet shop.py
@@ -1,3 +1,5 @@
+#1:
+
 import unittest
 from selenium import webdriver
 from LoginPage import LoginPage
@@ -38,6 +40,84 @@ class TestShop(unittest.TestCase):
         self.driver.quit()
 
 if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
+
+#2:
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDraverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class MainPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self):
+        self.driver.get("https://www.saucedemo.com/")
+
+    def login_as_standart_user(self):
+        username_input = self.driver.find_element(By.ID, "user-name")
+        username_input.send_keys("standart_user")
+
+        password_input = self.driver.find_element(By.ID, "password")
+        password_input.send_keys("secret_sauce")
+
+        login_button = self.driver.find_element(By.ID, "login-button")
+        login_button.click()
+
+
+
+class CartPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def add_to_cart(self, product_name):
+        add_to_cart_button = self.driver.find_element(By.XPATH, f"//div[@class='inventory_item_name'][text()='{product_name}']/following-sibling::button")
+        add_to_cart_button.click()
+
+    def checkout(self):
+        checkout_button = self.driver.find_element(By.ID, "checkout")
+        checkout_button.click()
+
+
+
+import unittest
+from selenium import webdriver
+from MainPage import MainPage
+from CartPage import CartPage
+
+class ShoppingCartTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.main_page = Main_page(self.driver)
+        self.cart_page = Cart_page(self.driver)
+
+    def test_shopping_cart(self):
+        self.main_page.open()
+        self.main_page.login_as_standart_user()
+
+        products = ["Sauce Labs Backpack", "Sauce Labs Bolt T-Shirt", "Sauce Labs Onesie"]
+
+        for product in products:
+            self.cart_page.add_to_cart(product)
+
+        self.cart_page.checkout()
+
+        total_price = self.driver.find_element(By.CLASS_NAME, "summary_total_label").text
+        self.assertEqual(total_price, "$58.29")
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
     unittest.main()
 
 
